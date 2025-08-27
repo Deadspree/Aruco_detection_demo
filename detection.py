d692c3e640fb2157b23b6da651e1c195ed30ba45
Deadspree <minhc613@gmail.com>
2025-08-27 19:40:12 +0700

    Fixed secondary commit

--- detection.py
+++ detection.py
@@ -1,34 +1,42 @@
+"""
+Aruco Marker detection
+
+Usage:
+    detection.py INPUT_PATH OUTPUT_PATH
+
+Options:
+
+-h --help             show this screen
+"""
 # Standard Library
 import time
 # External Library
 import cv2
 import numpy as np
+from docopt import docopt
 
 def main():
-    # Load the image
+    args = docopt(__doc__)
+    input = args['INPUT_PATH']
+    output = args['OUTPUT_PATH']
     start = time.time()
-    image = cv2.imread(r'input/singlemarkersoriginal.jpg')
-
-
-    # Convert the image to grayscale
+    image = cv2.imread(input)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
     parameters = cv2.aruco.DetectorParameters()
 
-
-    # Create the ArUco detector
     detector = cv2.aruco.ArucoDetector(aruco_dict, parameters)
-    # Detect the markers
+
     corners, ids, rejected = detector.detectMarkers(gray)
     end = time.time()
-    # Print the detected markers
+
     print("Detected markers:", ids)
     print("Detection time: {:.4f} seconds".format(end - start))
     if ids is not None:
         cv2.aruco.drawDetectedMarkers(image, corners, ids)
         cv2.imshow('Detected Markers', image)
-        cv2.imwrite(r'C:\Users\Admin_PC\Desktop\robot\aruco_marker\output\output_image.png', image)
+        cv2.imwrite(output, image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
--- marker_generation.py
+++ marker_generation.py
@@ -5,24 +5,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    # Define the dictionary we want to use
     aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
-
-    # Generate a marker
     marker_id = 23
-    marker_size = 200  # Size in pixels
+    marker_size = 200  
     marker_image = cv2.aruco.generateImageMarker(aruco_dict, marker_id, marker_size)
-
-    # Add a white border around the marker
-    border_size = 20  # pixels
+    border_size = 20  
     marker_with_border = cv2.copyMakeBorder(
         marker_image,
         border_size, border_size, border_size, border_size,
         cv2.BORDER_CONSTANT,
-        value=255  # white
+        value=255  
     )
 
-    # Save and display
     cv2.imwrite('marker_23_border.png', marker_with_border)
 
     plt.imshow(marker_with_border, cmap='gray', interpolation='nearest')
